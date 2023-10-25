9bc4ee7d394b32b13ca4ca03328283e57ad93f33
Mark Nolan <mnolan@shimmersensing.com>
2023-10-25 16:11:39 +0100

    enabling HW flow control

--- test_proj/dev_CYW20820/Core/Src/main.c
+++ test_proj/dev_CYW20820/Core/Src/main.c
@@ -716,7 +716,7 @@ void usart2UartUpdate(void)
   huart2.Init.StopBits = UART_STOPBITS_1;
   huart2.Init.Parity = UART_PARITY_NONE;
   huart2.Init.Mode = UART_MODE_TX_RX;
-  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_RTS_CTS;
   huart2.Init.OverSampling = UART_OVERSAMPLING_16;
   huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
   huart2.Init.ClockPrescaler = UART_PRESCALER_DIV1;
