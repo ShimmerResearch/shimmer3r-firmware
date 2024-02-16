949987e889782013c9493fa5b97823a8465e9899
Mark Nolan <marknolan@users.noreply.github.com>
2024-02-16 09:12:06 +0000

    Update rtc.c

--- S3R_Production/Core/Src/rtc.c
+++ S3R_Production/Core/Src/rtc.c
@@ -263,11 +263,12 @@ void S4_RTC_WakeUpSet(uint16_t period){
   /* HAL_StatusTypeDef HAL_RTCEx_SetWakeUpTimer_IT(RTC_HandleTypeDef *hrtc, uint32_t WakeUpCounter, uint32_t WakeUpClock,
    uint32_t WakeUpAutoClr)
    uint32_t WakeUpAutoClr missing argument in the old Shimmer4*/
-#if IS_SHIMMER3R if (HAL_RTCEx_SetWakeUpTimer_IT(&hrtc, prescalar, RTC_WAKEUPCLOCK_RTCCLK_DIV2, 0) != HAL_OK)
-  {
-#else if (HAL_RTCEx_SetWakeUpTimer_IT(&hrtc, prescalar, RTC_WAKEUPCLOCK_RTCCLK_DIV2) != HAL_OK)
-  {
+#if IS_SHIMMER3R 
+  if (HAL_RTCEx_SetWakeUpTimer_IT(&hrtc, prescalar, RTC_WAKEUPCLOCK_RTCCLK_DIV2, 0) != HAL_OK)
+#else
+  if (HAL_RTCEx_SetWakeUpTimer_IT(&hrtc, prescalar, RTC_WAKEUPCLOCK_RTCCLK_DIV2) != HAL_OK)
 #endif
+  {
     Error_Handler();
   }
 }
