07e8c12197f6bbfca0bcd04995f2282bc80bc0a1
Mark Nolan <mnolan@shimmersensing.com>
2024-05-14 15:06:22 +0100

    SHIM3-332 #time 5m missing header file declaration

--- S3R_Production/Shimmer_Driver/LIS2MDL/lis2mdl.h
+++ S3R_Production/Shimmer_Driver/LIS2MDL/lis2mdl.h
@@ -12,6 +12,7 @@ void lis2mdl_self_test(void);
 void lis2mdl_driver_init(void);
 void lis2mdl_set_default_config(void);
 void lis2mdl_config_mag(uint8_t rate, uint8_t range);
+HAL_StatusTypeDef lis2mdl_mag_get(uint8_t *buf);
 void lis2mdl_sleep(void);
 
 #endif /* LIS2MDL_LIS2MDL_H_ */
