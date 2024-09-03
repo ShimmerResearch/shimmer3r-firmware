d959503b6dc8a635339a765c73b974e095d8dc8a
Mark Nolan <mnolan@shimmersensing.com>
2024-09-03 09:07:00 +0100

    new IC test

--- S3R_Production/Shimmer_Driver/hal_FactoryTest.c
+++ S3R_Production/Shimmer_Driver/hal_FactoryTest.c
@@ -20,7 +20,8 @@ uint32_t run_factory_test(void)
   send_test_report("//**************************** TEST START "
                    "************************************//\r\n");
 
-  if (factoryTestToRun == FACTORY_TEST_MAIN)
+  if (factoryTestToRun == FACTORY_TEST_MAIN
+      || factoryTestToRun == FACTORY_TEST_ICS)
   {
     print_date_and_time();
     send_test_report("\r\n");
@@ -29,12 +30,19 @@ uint32_t run_factory_test(void)
     send_test_report("\r\n");
   }
 
-  if (factoryTestToRun == FACTORY_TEST_MAIN || factoryTestToRun == FACTORY_TEST_LEDS)
+  if (factoryTestToRun == FACTORY_TEST_MAIN
+      || factoryTestToRun == FACTORY_TEST_LEDS)
   {
     led_test();
   }
 
   if (factoryTestToRun == FACTORY_TEST_MAIN)
+  {
+    send_test_report("\r\n");
+  }
+
+  if (factoryTestToRun == FACTORY_TEST_MAIN
+      || factoryTestToRun == FACTORY_TEST_ICS)
   {
     send_test_report("\r\n");
 
