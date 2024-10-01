f474f65754bb21c0ecc4ed55747ff70cfb58581f
Mark Nolan <mnolan@shimmersensing.com>
2024-10-01 16:17:21 +0100

    SHIM3-338 #time 5m addressing PR comment

--- S3R_Production/Core/Src/i2c.c
+++ S3R_Production/Core/Src/i2c.c
@@ -519,7 +519,7 @@ void I2C_startSensing(void)
     }
     if (configBytes->chEnMag)
     {
-      LSM303DLHC_magInit(configBytes->magRateLsb, //sampling rate
+      LSM303DLHC_magInit(get_config_byte_mag_rate(), //sampling rate
           configBytes->magRange);                 //gain
     }
   }
