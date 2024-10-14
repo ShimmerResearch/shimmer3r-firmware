c3563dadec7f3aa349115cae189120e0b1b8a38a
Mark Nolan <mnolan@shimmersensing.com>
2024-10-14 15:53:06 +0100

    SHIM3-354 #time 5m resolve IOC file issue

--- S3R_Production/Core/Inc/usart.h
+++ S3R_Production/Core/Inc/usart.h
@@ -22,32 +22,31 @@
 #define __USART_H__
 
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
 
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
-  /* USER CODE BEGIN Includes */
+/* USER CODE BEGIN Includes */
 
 #include "shimmer_definitions.h"
 #include <shimmer_include.h>
 
-  /* USER CODE END Includes */
+/* USER CODE END Includes */
 
-  extern UART_HandleTypeDef huart1;
+extern UART_HandleTypeDef huart1;
 
-  extern UART_HandleTypeDef huart3;
+extern UART_HandleTypeDef huart3;
 
-  /* USER CODE BEGIN Private defines */
+/* USER CODE BEGIN Private defines */
 
-  /* USER CODE END Private defines */
+/* USER CODE END Private defines */
 
-  void MX_USART1_UART_Init(void);
-  void MX_USART3_UART_Init(void);
+void MX_USART1_UART_Init(void);
+void MX_USART3_UART_Init(void);
 
-  /* USER CODE BEGIN Prototypes */
+/* USER CODE BEGIN Prototypes */
 
   void setUartPeripheralPointers(void);
 
@@ -88,10 +87,11 @@ extern "C"
   void DockUart_writeBlocking(uint8_t *buf, uint8_t len);
   void DockUart_writeText(char *str);
 
-  /* USER CODE END Prototypes */
+/* USER CODE END Prototypes */
 
 #ifdef __cplusplus
 }
 #endif
 
 #endif /* __USART_H__ */
+
