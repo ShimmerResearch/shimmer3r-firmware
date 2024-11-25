65753c170d251b0252814920709646c0e6998e48
Mark Nolan <mnolan@shimmersensing.com>
2024-11-25 10:05:18 +0000

    SHIM3-369 #time 1m reset driver formatting

--- S3R_Production/Shimmer_Driver/ADXL371/adxl371.h
+++ S3R_Production/Shimmer_Driver/ADXL371/adxl371.h
@@ -37,7 +37,7 @@
  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*******************************************************************************/
+ ******************************************************************************/
 
 #ifndef ADXL371_ADXL371_H_
 #define ADXL371_ADXL371_H_
