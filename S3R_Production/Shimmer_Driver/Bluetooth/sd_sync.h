1c42cd0357ccb8c26379716d48005bfafcf7441d
SuhasVarna <SuhasVarna@SHIM-LAP-026>
2025-01-22 10:02:40 +0000

    fixes based on review comments - version 3

--- S3R_Production/Shimmer_Driver/Bluetooth/sd_sync.h
+++ S3R_Production/Shimmer_Driver/Bluetooth/sd_sync.h
@@ -119,11 +119,6 @@ void startBtForSync(void);
 void CommTimerStart(void);
 void CommTimerStop(void);
 
-//connect to a specific device that was previously discovered
-uint8_t BT_connect(uint8_t *addr);
-//after this command is called there will be no link to the connected device
-uint8_t BT_disconnect(void);
-
 #if defined(SHIMMER3)
 uint16_t GetTA0(void);
 #endif
