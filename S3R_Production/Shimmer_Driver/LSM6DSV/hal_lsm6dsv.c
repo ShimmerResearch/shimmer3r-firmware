7d81f332bcdc4c15753b800986eff52d977e9096
SuhasVarna <SuhasVarna@SHIM-LAP-026>
2025-01-27 13:06:03 +0000

    code changes to make lsm6dsv interrupt pin work

--- S3R_Production/Shimmer_Driver/LSM6DSV/hal_lsm6dsv.c
+++ S3R_Production/Shimmer_Driver/LSM6DSV/hal_lsm6dsv.c
@@ -456,8 +456,9 @@ void lsm6dsv_configure(float shimmerSamplingFreq,
     lsm6dsv_xl_full_scale_set(&lsm6dsv_obj.Ctx, (lsm6dsv_xl_full_scale_t) rangeAccel);
   }
 
-  //TODO if chip sampling rate is lower than Shimmer sampling, enable pin
+  // if chip sampling rate is lower than Shimmer sampling, enable pin
   //interrupt to only read data from chip when it's ready
+  lsm6dsv_interrupt_mode_t mode_int;
   lsm6dsv_pin_int_route_t pin_int;
   pin_int.drdy_g_eis = 0;
   pin_int.drdy_temp = 0;
@@ -479,17 +480,22 @@ void lsm6dsv_configure(float shimmerSamplingFreq,
   isDrdyIntEnabled = false;
   if (lsm6dsv_is_shimmer_freq_higher(shimmerSamplingFreq, rate))
   {
-    if ((isGyroEn && isAccelEn) || isGyroEn)
-    {
-      pin_int.drdy_g = PROPERTY_ENABLE;
-      isDrdyIntEnabled = true;
+     if ((isGyroEn && isAccelEn) || isGyroEn)
+     {
+        pin_int.drdy_g = PROPERTY_ENABLE;
+        mode_int.enable = PROPERTY_ENABLE;
+        mode_int.lir = PROPERTY_ENABLE;
+        isDrdyIntEnabled = true;
     }
     else
     {
-      pin_int.drdy_xl = PROPERTY_ENABLE;
-      isDrdyIntEnabled = true;
+        pin_int.drdy_xl = PROPERTY_ENABLE;
+        mode_int.enable = PROPERTY_ENABLE;
+        mode_int.lir = PROPERTY_ENABLE;
+        isDrdyIntEnabled = true;
     }
-    lsm6dsv_pin_int1_route_set(&lsm6dsv_obj.Ctx, &pin_int);
+      lsm6dsv_interrupt_enable_set(&lsm6dsv_obj.Ctx,mode_int);
+      lsm6dsv_pin_int1_route_set(&lsm6dsv_obj.Ctx, &pin_int);
   }
 
   ///* Configure filtering chain */
