6b3bd23e677c42c577929214aa765963fde121f9
Bhavana Rallapalli <brallapalli@shimmersensing.com>
2025-02-19 07:27:18 +0000

    Revert "DEV-174 #time 5h modified BT_connect/disconnect and cancel functions"
    This reverts commit 16a2441e91b64fa8ed7e4f1d2c5f05afc0b6144e.

--- S3R_Production/Shimmer_Driver/Bluetooth/CYW20820.h
+++ S3R_Production/Shimmer_Driver/Bluetooth/CYW20820.h
@@ -101,6 +101,4 @@ void setBtConnectionState(bool state);
 uint8_t BT_connect(uint8_t *addr);
 //after this command is called there will be no link to the connected device
 uint8_t BT_disconnect(void);
-// cancel is only used to cancel a pending connection attempt previously attempted by connect
-uint8_t BT_cancelConnection(void);
 #endif /* SRC_CYW20820_H_ */
