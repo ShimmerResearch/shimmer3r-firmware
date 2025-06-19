15ea5539abcabfbf42bc3b6dff90bbc20763ca1e
Mark Nolan <mnolan@shimmersensing.com>
2025-06-19 17:02:58 +0100

    DEV-28 #time 1h jump to BSL if btn is held for 3 sec during boot

--- S3R_Production/Core/Src/main.c
+++ S3R_Production/Core/Src/main.c
@@ -123,6 +123,8 @@ void Init()
   Board_ledTimersStart(&htim3, &htim2, &htim6);
 #endif
 
+  JumpToBootloaderIfRequired();
+
   setBootStage(BOOT_STAGE_START);
 
   ShimBrd_setHwId(DEVICE_VER);
@@ -259,8 +261,6 @@ int main(void)
 
   /* Check nBOOT0 option byte is configured correctly */
   checknBoot0OptionByte();
-
-  JumpToBootloaderIfRequired();
 
   //setup_factory_test(PRINT_TO_DEBUGGER, FACTORY_TEST_MAIN);
   //setup_factory_test(PRINT_TO_DEBUGGER, FACTORY_TEST_ICS);
@@ -701,35 +701,6 @@ void JumpToBootloaderIfRequired(void)
     }
     startLedBlinkTimer();
   }
-
-  //if(HAL_GPIO_ReadPin(USER_BTN_GPIO_Port, USER_BTN_Pin))
-  //{
-  //  stopLedBlinkTimer();
-  //  uint8_t bslCheckCounter = 0U;
-  //  do
-  //  {
-  //    if (bslCheckCounter % 2 == 0)
-  //    {
-  //      Board_ledUprSetColourRgb(0, 0, 0);
-  //      Board_ledLwrSetColourRgb(128, 0, 128);
-  //    }
-  //    else
-  //    {
-  //      Board_ledUprSetColourRgb(128, 0, 128);
-  //      Board_ledLwrSetColourRgb(0, 0, 0);
-  //    }
-  //
-  //    if (bslCheckCounter++ > 30U)
-  //    {
-  ////        SHIMMER_PRINTF("Entering bootloader mode\r\n");
-  //JumpToBootloader();
-  //}
-  ////Check if the user button is pressed to enter bootloader mode
-  //HAL_Delay(100U); //Wait 100ms before checking again
-  //} while (HAL_GPIO_ReadPin(USER_BTN_GPIO_Port, USER_BTN_Pin));
-  //
-  //startLedBlinkTimer();
-  //}
 }
 
 /*Temporarily located in main.c. Based on the following example */
