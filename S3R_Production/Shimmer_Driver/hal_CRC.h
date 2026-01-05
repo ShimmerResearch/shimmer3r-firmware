7c4c788b109d663e1e22477451fedc32938f953a
Mark Nolan <mnolan@shimmersensing.com>
2026-01-05 15:38:39 +0000

    DEV-621 #time 10m move most CRC code to common repo

--- S3R_Production/Shimmer_Driver/hal_CRC.h
+++ S3R_Production/Shimmer_Driver/hal_CRC.h
@@ -11,22 +11,6 @@
 #include "stm32u5xx.h"
 #include <stdint.h>
 
-#define CRC_INIT 0xB0CA
-
-// CRC modes - the ordinal corresponds to the number of CRC bytes
-typedef enum
-{
-  CRC_OFF,
-  CRC_1BYTES_ENABLED,
-  CRC_2BYTES_ENABLED,
-  CRC_MAX_SUPPORTED_BYTES
-} COMMS_CRC_MODE;
-
-uint32_t CRC_data(uint8_t *buf, uint8_t len);
-void calculateCrcAndInsert(uint8_t crcMode, uint8_t *aryPtr, uint8_t len);
-uint8_t checkCrc(uint8_t crcMode, uint8_t *aryPtr, uint8_t payloadLen);
-
 void setCrcHandleToUse(CRC_HandleTypeDef *hcrc);
 
-uint8_t testCrcDriver(void);
 #endif /* HAL_CRC_H_ */
